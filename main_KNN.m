903e3225a911653ae388b96247c099bce55261f6
pablomarino07 <pablomarino56@gmail.com>
2024-06-13 18:58:23 -0300

    modificacion_para_noramlizar

--- main_KNN.m
+++ main_KNN.m
@@ -2,7 +2,7 @@ function m = main_KNN()
   pkg load statistics;
 
   % Cargar los datos de entrenamiento
-  X_train = cargar_en_matriz_caracterisitcas_6s_15();
+  [X_train,max_min] = cargar_en_matriz_caracterisitcas();
   Y_train = zeros(1, 120);  % Vector de etiquetas de longitud 120 lleno de ceros
   Y_train(1:60) = 1;
   Y_train(61:end) = 0;
@@ -13,18 +13,27 @@ function m = main_KNN()
 
  for j=1:15
     % Cargar datos de prueba
-    filename = sprintf('C:\\Users\\pablo\\Desktop\\Trabajo Señales\\TpFinal\\Base de Datos EEG\\Pruebas\\solo_sesion3\\honest\\honest_subject%d_session1_probe_probe.txt',j,j);
-    filename1 = sprintf('C:\\Users\\pablo\\Desktop\\Trabajo Señales\\TpFinal\\Base de Datos EEG\\Pruebas\\solo_sesion3\\lying\\lying_subject%d_session1_probe.txt',j,j);
+    filename = sprintf('C:\\Users\\pablo\\Desktop\\Trabajo Señales\\TpFinal\\Base de Datos EEG\\Pruebas\\solo_sesion1\\honest\\honest_subject%d_session1_probe.txt',j);
+    filename1 = sprintf('C:\\Users\\pablo\\Desktop\\Trabajo Señales\\TpFinal\\Base de Datos EEG\\Pruebas\\solo_sesion1\\lying\\lying_subject%d_session1_probe.txt',j);
 
     data = load(filename);
     data1 = load(filename);
     fm = 500;  % Frecuencia de muestreo
     caract = extraer_caractersiticas_NB(data, fm, 3);
     caract1 = extraer_caractersiticas_NB(data1, fm, 3);
-    caract_vector = caract(:);
-    X_test = caract_vector';
-    caract_vector1 = caract1(:);
-    X_test1 = caract_vector1';
+  for m=1:39
+
+      maximo=max_min(1,m);
+      minimo=max_min(2,m);
+      caract(m)=(aux-minimo)/(maximo-minimo);
+      caract1(m)=(aux-minimo)/(maximo-minimo);
+
+
+  endfor
+
+    X_test = caract;
+
+    X_test1 = caract1;
 
 
 
